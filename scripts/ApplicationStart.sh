46e69c241ac5155a0cdc7655ed8475a08dc727ff
lfw960203 <lfw960203@gmail.com>
2025-12-26 10:38:16 +0800

    ver5.0 first commit

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -p 5000:5000 --name flask-devops -d 129729052534.dkr.ecr.ap-northeast-1.amazonaws.com/iii-devops-ecr:latest
+docker run -p 80:5000 --name flask-web -d 827350540951.dkr.ecr.ap-northeast-1.amazonaws.com/cjc102-04-ecr-repo:latest
+
+sleep 20s
--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-$(aws ecr get-login --no-include-email --registry-ids 129729052534 --region ap-northeast-1)
+aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 827350540951.dkr.ecr.ap-northeast-1.amazonaws.com
 
-if [ `docker images | grep iii-devops-ecr | wc -l`  = 1 ]
+if [ `docker images | grep 827350540951.dkr.ecr.ap-northeast-1.amazonaws.com/cjc102-04-ecr-repo | wc -l`  = 1 ]
 then
-        docker rmi iii-devops-ecr
-        docker pull 129729052534.dkr.ecr.ap-northeast-1.amazonaws.com/iii-devops-ecr:latest
+        docker rmi 827350540951.dkr.ecr.ap-northeast-1.amazonaws.com/cjc102-04-ecr-repo
+        docker pull 827350540951.dkr.ecr.ap-northeast-1.amazonaws.com/cjc102-04-ecr-repo:latest
 else
-        docker pull 129729052534.dkr.ecr.ap-northeast-1.amazonaws.com/iii-devops-ecr:latest
+        docker pull 827350540951.dkr.ecr.ap-northeast-1.amazonaws.com/cjc102-04-ecr-repo:latest
 fi
